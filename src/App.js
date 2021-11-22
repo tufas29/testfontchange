ae0493c167c3176fa265915721c6f2e82bb83d11
Shmuel Tufahi <tufas29@gmail.com>
2021-11-22 15:09:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ברוכים הבאים לאתר שלי</h1>
+        <button className="btn">לחץ עליי!</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית סחטיר בלובק.
+          תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם
+          גדדיש. קוויז דומור ליאמום בלינך חקיגו רוגצה.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
